53be0458a2ccb05f9d29a2663eb3e7186ee34a28
unname <lwer210@naver.com>
2024-12-09 01:27:09 +0900

    이메일 전송 응답 객체

---
+++ src/main/java/com/example/english/auth/controller/response/EmailSendResponse.java
@@ -0,0 +1,24 @@
+package com.example.english.auth.controller.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class EmailSendResponse {
+
+    private String authCode;
+
+    private LocalDateTime issuedAt;
+
+    private LocalDateTime expiresAt;
+}
