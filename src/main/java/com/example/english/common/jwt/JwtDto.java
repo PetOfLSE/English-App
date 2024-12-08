f8c63a54213614690693eb6f46302f18d103d425
unname <lwer210@naver.com>
2024-12-09 01:29:42 +0900

    Jwt 토큰 응답 객체

---
+++ src/main/java/com/example/english/common/jwt/JwtDto.java
@@ -0,0 +1,19 @@
+package com.example.english.common.jwt;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class JwtDto {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
